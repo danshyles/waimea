129c104d41b1b84d3b7422e914657030b931183d
Dan Shyles <daniel.shyles@gmail.com>
2019-06-06 13:55:11 -1000

    updated thermistor.py with exponential model and steinhart equation for calculating temp

--- thermistor/thermistor.py
+++ thermistor/thermistor.py
@@ -2,6 +2,7 @@ import busio
 import digitalio
 import board
 import time
+import math
 import adafruit_mcp3xxx.mcp3008 as MCP
 from adafruit_mcp3xxx.analog_in import AnalogIn
  
@@ -17,10 +18,18 @@ mcp = MCP.MCP3008(spi, cs)
 # create an analog input channel on pin 0
 chan = AnalogIn(mcp, MCP.P0)
 
+#Steinhart-Hart Equation method of determining temperature
+def steinhart_temperature_C(r, Ro=10000.0, To=25.0, beta=3950.0):
+    steinhart = math.log(r/Ro)/beta
+    steinhart += 1.0/(To+273.15)
+    steinhart = (1.0/steinhart)-273.15
+    return steinhart
+
 while True:
     print('Raw ADC Value: ', chan.value)
     print('ADC Voltage: ' + str(chan.voltage) + 'V')
-    resistance = 10000/((3.3/chan.voltage)-1)
+    resistance = 10000/((65535/chan.value)-1)
     print('resistance: ' + str(resistance) + 'Ohms')
-    print('Temp: ' + str(chan.voltage*100-50) + 'C')
+    print('Temp: ' + str(math.log((resistance/(30.6881*1000)), 0.956118)) + 'C')
+    print('Steinhart Temp: ' + str(steinhart_temperature_C(resistance)) + 'C')
     time.sleep(0.5)
